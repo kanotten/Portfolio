04105f9f6a979402b1081fe2ce82368df70d6c07
Sheikh <kenneth.sheikh@hotmail.com>
2024-06-07 06:02:51 +0200

    added needed code into project.js and fixed body styling to all project pages

--- JS/project.js
+++ JS/project.js
@@ -1,17 +1,48 @@
-// Bootsrap libs
-document.write(
-  '<script src="https://kit.fontawesome.com/3b57a2a2a7.js"></script>',
-);
-document.write(
-  '<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>',
-);
-document.write(
-  '<script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>',
-);
-document.write(
-  '<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>',
-);
-document.write(
-  '<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>',
-);
-document.write('<script src="JS/app.js"></script>');
+// loading of JavaScript libraries
+function loadScript(url, callback) {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = url;
+  script.onload = callback;
+  document.head.appendChild(script);
+}
+
+// Load jQuery dynamically
+loadScript("https://code.jquery.com/jquery-3.5.1.min.js", function () {
+  // jQuery loaded, now execute your code
+  $(document).ready(function () {
+    $(window).scroll(function () {
+      // Sticky navbar on scroll script
+      if (this.scrollY > 20) {
+        $(".navbar").addClass("sticky");
+      } else {
+        $(".navbar").removeClass("sticky");
+      }
+
+      // Scroll-up button show/hide script
+      if (this.scrollY > 500) {
+        $(".scroll-up-btn").addClass("show");
+      } else {
+        $(".scroll-up-btn").removeClass("show");
+      }
+    });
+
+    // Slides up
+    $(".scroll-up-btn").click(function () {
+      $("html").animate({ scrollTop: 0 });
+      // Removing smooth scroll on slide-up button click
+      $("html").css("scrollBehavior", "auto");
+    });
+
+    $(".navbar .menu li a").click(function () {
+      // Scroll on Menu Items click
+      $("html").css("scrollBehavior", "smooth");
+    });
+
+    // Toggle Navbar
+    $(".menu-btn").click(function () {
+      $(".navbar .menu").toggleClass("active");
+      $(".menu-btn i").toggleClass("active");
+    });
+  });
+});
